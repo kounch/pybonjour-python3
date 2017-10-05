6ea2d607c996d75de8aadd72dba2f872e5ddcdf6
Timoteus Ruotsalainen <timoteus.ruotsalainen@iki.fi>
2017-10-05 20:16:32 +0300

    update to python3 and style fixes

--- examples/register.py
+++ examples/register.py
@@ -2,24 +2,23 @@ import select
 import sys
 import pybonjour
 
-
-name    = sys.argv[1]
+name = sys.argv[1]
 regtype = sys.argv[2]
-port    = int(sys.argv[3])
+port = int(sys.argv[3])
 
 
 def register_callback(sdRef, flags, errorCode, name, regtype, domain):
     if errorCode == pybonjour.kDNSServiceErr_NoError:
-        print 'Registered service:'
-        print '  name    =', name
-        print '  regtype =', regtype
-        print '  domain  =', domain
+        print('Registered service:')
+        print('  name    =', name)
+        print('  regtype =', regtype)
+        print ('  domain  =', domain)
 
 
-sdRef = pybonjour.DNSServiceRegister(name = name,
-                                     regtype = regtype,
-                                     port = port,
-                                     callBack = register_callback)
+sdRef = pybonjour.DNSServiceRegister(name=name,
+                                     regtype=regtype,
+                                     port=port,
+                                     callBack=register_callback)
 
 try:
     try:
