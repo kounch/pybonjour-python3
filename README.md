b45426106286df3b268fdc9be010a791f859f3f8
kounch <kounch@users.noreply.github.com>
2019-03-05 21:21:41 +0100

    Added LICENSE. New setup.py. Version 1.1.3

--- README.md
+++ README.md
@@ -1,37 +1,27 @@
-[![Build Status](https://travis-ci.org/TimoteusRuotsalainen/pybonjour-python3.svg?branch=master)](https://travis-ci.org/TimoteusRuotsalainen/pybonjour-python3)
-----------------------------------------------------
-pybonjour:  Pure-Python interface to Apple Bonjour
+# pybonjour
 
-Author: Christopher Stawarz <cstawarz@csail.mit.edu>
-Author: Wim Haanstra <wim@wim.me>
-Author: Timoteus Ruotsalainen <timoteus.ruotsalainen@iki.fi>
+Pure-Python interface to Apple Bonjour.
+
+Author: [Christopher Stawarz](cstawarz@csail.mit.edu)
+Author: [Wim Haanstra](wim@wim.me)
+Author: [Timoteus Ruotsalainen](timoteus.ruotsalainen@iki.fi)
 Version: 1.1.2
 October 5, 2017
+
 ----------------------------------------------------
 
+## Introduction
 
-Introduction
-------------
+This is based on the fork of Timoteus Ruotsalainen fork from [https://github.com/depl0y/pybonjour-python3](https://github.com/depl0y/pybonjour-python3) who forked the code from [https://code.google.com/archive/p/pybonjour/](https://code.google.com/archive/p/pybonjour/)
 
-This is my fork from https://github.com/depl0y/pybonjour-python3
-who forked the code from https://code.google.com/archive/p/pybonjour/
+[Ryan McGuire](https://github.com/EnigmaCurry) made some updates regarding python3 in 2013 and I have added some more fixes for it to work with python3.
 
-Ryan McGuire (https://github.com/EnigmaCurry) made some updates regarding 
-python3 in 2013 and I have added some more fixes for it to work with python3.
-
-pybonjour provides a pure-Python interface (via ctypes) to Apple
-Bonjour and compatible DNS-SD libraries (such as Avahi).  It allows
-Python scripts to take advantage of Zero Configuration Networking
-(Zeroconf) to register, discover, and resolve services on both local
-and wide-area networks.  Since pybonjour is implemented in pure
-Python, scripts that use it can easily be ported to Mac OS X, Windows,
-Linux, and other systems that run Bonjour.
+pybonjour provides a pure-Python interface (via ctypes) to Apple Bonjour and compatible DNS-SD libraries (such as Avahi).  It allows Python scripts to take advantage of Zero Configuration Networking (Zeroconf) to register, discover, and resolve services on both local and wide-area networks.  Since pybonjour is implemented in pure Python, scripts that use it can easily be ported to Mac OS X, Windows, Linux, and other systems that run Bonjour.
 
 pybonjour is free software, distributed under the MIT license.
 
+## Requirements
 
-Requirements
-------------
+pybonjour requires Python 2.7 or later. It also depends on ctypes (version 1.0.1 or later), which is part of the standard library in Python 2.5 and later.
 
-Documentation
--------------
+No additional software is required to use pybonjour under Mac OS X. To use it under Windows, the Bonjour for Windows package must be installed on your system. Most Linux systems use Avahi rather than Bonjour for DNS-SD. If this is the case on your system, you'll need to install Avahi's Bonjour compatibility library. (Under Ubuntu, the package to install is libavahi-compat-libdnssd1.) Otherwise, or to use pybonjour under other POSIX systems, you must download, compile, and install Bonjour from source.
--- setup.py
+++ setup.py
@@ -1,59 +1,45 @@
 #!/usr/bin/env python
 
-################################################################################
-#
-# Copyright (c) 2007-2008 Christopher J. Stawarz
-#
-# Permission is hereby granted, free of charge, to any person
-# obtaining a copy of this software and associated documentation files
-# (the "Software"), to deal in the Software without restriction,
-# including without limitation the rights to use, copy, modify, merge,
-# publish, distribute, sublicense, and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so,
-# subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT.  IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
-# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
-# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-#
-################################################################################
+# This file is licensed under the terms of the MIT License.
+# See the LICENSE file in the root of this repository for complete details.
 
+from __future__ import with_statement
 
-from distutils.core import setup
+import sys
+import os
+from setuptools import setup
+
 import pybonjour
 
+base_dir = os.path.dirname(__file__)
 
-# Grab the description from the package's doc string
-desc = pybonjour.__doc__.strip().split('\n\n')
+pybonjour_classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Operating System :: MacOS :: MacOS X",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX",
+    "Programming Language :: Python",
+    "Topic :: System :: Distributed Computing",
+    "Topic :: System :: Networking",
+]
 
+with open(os.path.join(base_dir, "README.md")) as f:
+    long_description = f.read()
 
 setup(
-    name='pybonjour',
+    name="pybonjour",
     version=pybonjour.__version__,
-    author='Christopher Stawarz',
-    author_email='cstawarz@csail.mit.edu',
-    url='http://o2s.csail.mit.edu/o2s-wiki/pybonjour',
-    description=desc[0].strip(),
-    long_description=desc[1].strip(),
-    download_url='http://o2s.csail.mit.edu/download/pybonjour/',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: MacOS :: MacOS X',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: POSIX',
-        'Programming Language :: Python',
-        'Topic :: System :: Distributed Computing',
-        'Topic :: System :: Networking',
-    ],
-    py_modules=['pybonjour'],
+    description=("pybonjour provides a pure-Python interface (via ctypes) to "
+               " Apple Bonjour and compatible DNS-SD libraries (like Avahi)."),
+    long_description=long_description,
+    license="MIT License",
+    url="http://o2s.csail.mit.edu/o2s-wiki/pybonjour",
+    py_modules=["pybonjour"],
+    author="Christopher J. Stawarz",
+    author_email="cstawarz@csail.mit.edu",
+    classifiers=pybonjour_classifiers,
+    python_requires='>=2.7,!=3.0.*,!=3.1.*,!=3.2.*,!=3.3.*',
 )
